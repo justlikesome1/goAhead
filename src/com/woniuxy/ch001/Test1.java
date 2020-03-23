95d59ff157a4c0cf68e4f4b95be38261302c03c6
justlikesome1 <541839425@qq.com>
2020-03-23 21:59:30 +0800

    修改

--- src/com/woniuxy/ch001/Test1.java
+++ src/com/woniuxy/ch001/Test1.java
@@ -3,5 +3,9 @@ package com.woniuxy.ch001;
 public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("lallalal");
+
+		System.out.println(3243);
+		System.out.println("能看见不");
 	}
+
 }
